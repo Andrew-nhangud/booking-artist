4e23744e797e7462aa952fe49e9c1b77d13734c8
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-05-05 15:54:17 +0200

    added react router to the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Landing } from "./pages/landing/Landing.jsx";
+import { Children, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/global.css";
 import App from "./App.jsx";
+import "./styles/global.css";
+import { Bookings } from "./pages/bookings/Bookings.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Landing /> },
+      { path: "/bookings", element: <Bookings /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
