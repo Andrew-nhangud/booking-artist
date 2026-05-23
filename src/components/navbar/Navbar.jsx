654ab827ee6482729e54ddea410b546725f80741
Andrew Nhangud <Andrewnhangud3785@gmail.com>
2026-05-23 13:08:11 +0200

    added comments

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,9 +5,11 @@ export const Navbar = () => {
   return (
     <nav className="navlinks container">
       <ul>
+        {/* Logo link */}
         <Link className="Link" to="/">
           <li className="logo">RÉSERVE</li>
         </Link>
+        {/* Bookings link */}
         <Link className="Link" to="/bookings">
           <div className="booking-container">
             <li className="bookings">Bookings</li>
